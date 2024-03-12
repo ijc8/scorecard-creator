18c53395b92b48106056517e61733c0655512470
Ian Clester <ijc@ijc8.me>
2024-03-11 23:41:51 -0400

    Update page title

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: "Emception",
+            title: "ScoreCard Creator",
         }),
         new MonacoWebpackPlugin({ languages: ["cpp"] }),
         new CopyWebpackPlugin({
